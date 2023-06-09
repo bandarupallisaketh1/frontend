4a6b5be9908fb1e057d25e870e94f07b125502f7
bandarupallisaketh1 <sakethchowdary112@gmail.com>
2023-06-09 11:55:27 +0530

    updated code

--- src/compnents/Navigation.js
+++ src/compnents/Navigation.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {Nav, Navbar, NavLink } from "react-bootstrap"
+import { Nav, Navbar, NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Navigation() {
-  return (
-    <div>
-        <Navbar collapseOnSelect expand="sn" bg="dark" variant="dark">
-            <Navbar.Toggle aria-aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navabrScroll"/>
+const Navigationbar = () => {
+    return (
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav>
-                    <NavLink eventKey="1" as={Link} to="/">ResistrationPage</NavLink>
-                    <NavLink eventKey="2" as={Link} to="/loginpage">LoginPage</NavLink>
-                    <NavLink eventKey="3" as={Link} to="/productspage">ProductsPage</NavLink>
+                    <NavLink  eventKey="1" as={Link} to="/">Home</NavLink>
+                    <NavLink  eventKey="2" as={Link} to="/logout">Logout</NavLink>
+                    <NavLink  eventKey="3" as={Link} to="/todo">todo</NavLink>
                 </Nav>
-            </Navbar.Collapse>
+            </Navbar.Collapse>     
         </Navbar>
-    </div>
-  )
+    );
 }
+ 
+export default Navigationbar;
